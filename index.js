1f388d373e12baf4e93700b6fdaedfb09a615c09
AGRawlins <83105062+agrawlins@users.noreply.github.com>
2021-06-30 21:36:25 -0400

    Add files via upload
    Baby steps; here we go!

---
+++ index.js
@@ -0,0 +1,9 @@
+//const {something} = require('./somethingElse'); <== just a formula for 
+
+const prompt = require('prompt-sync')();
+
+console.log('Hello, and welcome to Embezzlers (now Audited)! What would you like to do today?');
+
+const mainMenuItem = prompt();
+
+console
